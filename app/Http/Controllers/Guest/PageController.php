b04002485f54a5fb968ccf8e25b048d803484b01
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-20 15:48:16 +0100

    [ add ] empty error

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -11,12 +11,18 @@ class PageController extends Controller
     public function index()
     {
         $movies = Movie::all();
+        if (empty($movies)) {
+            abort(404);
+        }
         return view("home", compact("movies"));
     }
 
     public function detail($id)
     {
         $movie = Movie::find($id);
+        if (empty($movie)) {
+            abort(404);
+        }
         return view("movie_detail", compact("movie"));
     }
 }
